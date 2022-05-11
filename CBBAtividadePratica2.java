d4a14d56c25f3332962d8b9955b11609b5bcef4f
Jose-Igor <95245791+Jose-Igor@users.noreply.github.com>
2022-05-11 17:36:18 -0300

    Fazendo upload das questões Chagas, Baruque & Baruque 2 e 4.

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,10 @@
+public class CBBAtividadePratica2 {
+    public static void main(String[] args){
+        int primeiroNumero = 86;
+        int segundoNumero = 78;
+        int terceiroNumero = 94;
+        int soma = primeiroNumero + segundoNumero + terceiroNumero;
+        System.out.println("A soma é: " + soma);
+        System.out.println("A média é: " + soma/3);
+    }
+}
---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,7 @@
+public class CBBAtividadePratica4 {
+    public static void main(String[] args){
+        double cubagem = 22.5;
+        double valorTotal = cubagem * 100;
+        System.out.println("O valor custo da piscina será: R$" + valorTotal);
+    }
+}
