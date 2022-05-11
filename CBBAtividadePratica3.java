8794fb84fa76f2ff99a2138545d96b388e09ebfb
Diuari <95585201+Diuari@users.noreply.github.com>
2022-05-11 16:25:10 -0300

    CBBAtividadePrática3.java

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,14 @@
+public class CBBAtividadePratica3 {
+    
+    public static void main(String[] args){
+
+        int altura=3;
+        int base=4;
+
+        int area = base * altura;
+
+        System.out.println("A area do retangulo e " + area);
+        
+    }
+
+}
